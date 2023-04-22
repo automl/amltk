0f60f4e0dc22584bde254c92b186546787d96b5b
Eddie Bergman <eddiebergmanhs@gmail.com>
2023-04-22 08:18:34 +0200

    doc(sklearn): Pipeline builder and data splitting (#73)

--- src/byop/sklearn/__init__.py
+++ src/byop/sklearn/__init__.py
@@ -1,8 +1,8 @@
-from byop.sklearn.builder import build as build_pipeline
+from byop.sklearn.builder import build as sklearn_pipeline
 from byop.sklearn.data import split_data, train_val_test_split
 
 __all__ = [
     "train_val_test_split",
     "split_data",
-    "build_pipeline",
+    "sklearn_pipeline",
 ]
