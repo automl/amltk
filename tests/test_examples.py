c6558007bba4213208727bae21efa64f4267bb38
eddiebergman <eddiebergmanhs@gmail.com>
2023-11-24 01:34:46 +0100

    ci: Only test `"doc-runnable"` examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -9,9 +9,22 @@ HERE = Path(__file__).parent.resolve()
 EXAMPLE_DIR = HERE.parent / "examples"
 example_files = EXAMPLE_DIR.rglob("*.py")
 
+
+def is_runnable(example: Path) -> bool:
+    with example.open() as f:
+        # If `doc-runnable` is not in the first 10 lines,
+        # we don't consider it testable.
+        for _ in range(10):
+            line = next(f)
+            if "doc-runnable" in line.lower():
+                return True
+        return False
+
+
 parameters = [
     pytest.param(example, id=str(example.relative_to(EXAMPLE_DIR)))
     for example in example_files
+    if is_runnable(example)
 ]
 
 
